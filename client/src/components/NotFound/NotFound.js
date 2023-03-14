4e50a5e3a9d0349bfba69d221c85f664b21f72bf
Boroboysh <artyomklim001@gmail.com>
2023-03-14 14:27:20 +0300

    add: validation for points, jwt token authentication

---
+++ client/src/components/NotFound/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './internalServer.module.css';
+
+const InternalServer = (props) => {
+    return (
+        <p className={styles.internalServer}>{"500 SERVER ERROR!"}</p>
+    )
+}
+
+export default InternalServer;
